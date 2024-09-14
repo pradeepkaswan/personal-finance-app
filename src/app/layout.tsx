5f3bb4b206af4c18af0fe80d81adc52c602c26ce
Pradeep Kaswan <pradeepkaswan7@gmail.com>
2024-09-14 20:17:32 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const publicSans = localFont({
+  src: "../assets/fonts/PublicSans-VariableFont_wght.ttf",
+  variable: "--font-public-sans",
   weight: "100 900",
 });
 
@@ -25,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${publicSans.variable} antialiased bg-beige-100`}>
         {children}
       </body>
     </html>
