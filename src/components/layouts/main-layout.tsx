6d96d63d093408ff1dbba40f0c06510d7eebd2af
Pradeep Kaswan <pradeepkaswan7@gmail.com>
2024-10-20 02:31:43 +0530

    feat: complete bottom tab nav for mobile screens

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Icons } from "../ui/icons";
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useLocation } from "react-router-dom";
 
 const NAV_ITEMS = [
   {
@@ -37,23 +37,32 @@ const NAV_ITEMS = [
 
 export default function Layout() {
   const [isOpen, setIsOpen] = useState(true);
+  const location = useLocation();
 
-  // const toggleSidebar = () => setIsOpen(!isOpen);
+  const toggleSidebar = () => setIsOpen(!isOpen);
 
   return (
     <div className="h-screen flex">
       <aside
-        className={`${
+        className={`hidden xl:flex ${
           isOpen ? "w-[300px]" : "w-[88px]"
-        } bg-grey-900 px-10 py-[40px] h-screen flex flex-col rounded-r-2xl  transition-all duration-300 ease-in-out text-grey-300 text-preset-3`}
+        } bg-grey-900 py-[40px] flex flex-col rounded-r-2xl transition-all duration-300 ease-in-out text-grey-300 text-preset-3`}
       >
         {isOpen ? (
-          <img src="/images/logo-large.svg" className="w-[122px]" alt="" />
+          <img
+            src="/images/logo-large.svg"
+            className="w-fit pl-8"
+            alt="Logo Large"
+          />
         ) : (
-          <img src="/images/logo-small.svg" className="" alt="" />
+          <img
+            src="/images/logo-small.svg"
+            className="w-fit pl-9"
+            alt="Logo Small"
+          />
         )}
 
-        <nav className="mt-10 -mx-10">
+        <nav className="mt-10 ">
           <ul className="mt-6">
             {NAV_ITEMS.map((item) => (
               <li key={item.id}>
@@ -62,8 +71,10 @@ export default function Layout() {
                     <div
                       className={`flex flex-row items-center gap-4 mt-2 h-[56px] pl-6 ${
                         isActive
-                          ? "text-green bg-beige-100 rounded-r-xl mr-6 border-l-4 border-green"
-                          : "hover:text-grey-100"
+                          ? `text-green bg-beige-100 rounded-r-xl ${
+                              isOpen ? "mr-6" : "mr-4"
+                            } border-l-4 border-green`
+                          : "hover:text-grey-100 pl-7"
                       }`}
                     >
                       {item.icon}
@@ -84,9 +95,9 @@ export default function Layout() {
           </ul>
         </nav>
 
-        <footer className="mb-6 mt-auto">
+        <footer className="mb-6 mt-auto pl-8">
           <button
-            onClick={() => setIsOpen((prev) => !prev)}
+            onClick={toggleSidebar}
             aria-label={isOpen ? "Expand Menu" : "Collapse Menu"}
           >
             {isOpen ? (
@@ -102,9 +113,45 @@ export default function Layout() {
           </button>
         </footer>
       </aside>
-      <main className="flex-1 overflow-auto">
+      <main className="flex-1 overflow-auto m-4 md:m-10">
+        <h1 className="text-preset-1 text-grey-900 py-2">
+          {location.pathname.slice(1).charAt(0).toUpperCase() +
+            location.pathname.slice(2)}
+        </h1>
         <Outlet />
       </main>
+
+      <nav className="xl:hidden fixed bottom-0 left-0 right-0 bg-grey-900 text-grey-300 pt-2 px-4 rounded-t-xl">
+        <ul className="flex flex-row">
+          {NAV_ITEMS.map((item) => (
+            <li
+              key={item.id}
+              className="flex items-center justify-center w-full"
+            >
+              <NavLink to={item.href} className="w-full max-w-[104px]">
+                {({ isActive }) => (
+                  <div
+                    className={`flex flex-col items-center gap-1 justify-center py-2 ${
+                      isActive
+                        ? "text-green bg-beige-100 rounded-t-lg border-b-4 border-green"
+                        : "hover:text-grey-100"
+                    }`}
+                  >
+                    {item.icon}
+                    <span
+                      className={`hidden md:block text-preset-5 font-bold ${
+                        isActive ? "text-grey-900" : "hover:text-grey-100"
+                      }`}
+                    >
+                      {item.label}
+                    </span>
+                  </div>
+                )}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
--- src/pages/overview.tsx
+++ src/pages/overview.tsx
@@ -1,3 +1,36 @@
+const Card = ({
+  title,
+  amount,
+  isBalance,
+}: {
+  title: string;
+  amount: string;
+  isBalance: boolean;
+}) => (
+  <div
+    className={`flex flex-col gap-3 p-5 rounded-xl w-full ${
+      isBalance ? "bg-grey-900" : "bg-white"
+    }`}
+  >
+    <h2
+      className={`text-preset-4 ${isBalance ? "text-white" : "text-grey-500"}`}
+    >
+      {title}
+    </h2>
+    <p
+      className={`text-preset-1 ${isBalance ? "text-white" : "text-grey-900"}`}
+    >
+      ${amount}
+    </p>
+  </div>
+);
+
 export default function Overview() {
-  return <div>Overview</div>;
+  return (
+    <div className="mt-8 flex flex-col md:flex-row gap-3 md:gap-6">
+      <Card title="Current Balance" amount="4,836.00" isBalance={true} />
+      <Card title="Income" amount="3,814.25" isBalance={false} />
+      <Card title="Expense" amount="1,700.50" isBalance={false} />
+    </div>
+  );
 }
